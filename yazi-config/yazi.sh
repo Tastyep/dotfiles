5e1621489952a3697976eeee6f9165134ff99fea
Luc Sinet <luc.sinet@gmail.com>
2023-11-26 18:20:38 +0100

    feat: add yazi

---
+++ yazi-config/yazi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function ya() {
+	tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+	yazi --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
