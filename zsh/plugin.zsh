c5f8f8f33b4668caaf749bc37e0dc83347ff97ff
Luc Sinet <luc.sinet@gmail.com>
2023-11-28 15:31:03 +0100

    feat: add you-should-use

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -1,4 +1,4 @@
 # zinit plugins
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 zinit ice depth=1; zinit light Aloxaf/fzf-tab
-
+zinit ice depth=1; zinit light MichaelAquilina/zsh-you-should-use
