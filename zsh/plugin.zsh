f76e8fa3b2d80480604e0fa4583d6d5cbbe3ae54
Luc Sinet <luc.sinet@gmail.com>
2023-12-01 18:26:19 +0100

    feat: add zsh-eza plugin

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -2,3 +2,4 @@
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 zinit ice wait depth=1; zinit light Aloxaf/fzf-tab
 zinit ice wait depth=1; zinit light MichaelAquilina/zsh-you-should-use
+zinit ice wait depth=1; zinit light z-shell/zsh-eza
