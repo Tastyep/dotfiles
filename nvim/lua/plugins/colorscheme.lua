c7becb922ee202ae483623e3ea3b1ad94f668287
Luc Sinet <luc.sinet@gmail.com>
2024-01-02 21:46:20 +0100

    style(nvim): try melange color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
   -- add colorscheme
-  { "tanvirtin/monokai.nvim" },
+  -- { "tanvirtin/monokai.nvim" },
   { "sainnhe/sonokai" },
   { "savq/melange-nvim" },
+  -- { "ray-x/starry.nvim" },
   -- Configure LazyVim to load the colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "sonokai",
       colorscheme = "melange",
+      -- colorscheme = "monokai",
     },
   },
 }
