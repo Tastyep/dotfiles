38b459d5e63d3b49ae385f4b1424116f6aeb61f5
Luc Sinet <luc.sinet@gmail.com>
2024-01-02 12:04:24 +0100

    feat(plugins): add zsh-completions

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -3,3 +3,4 @@ zinit ice depth=1; zinit light romkatv/powerlevel10k
 zinit ice wait depth=1; zinit light Aloxaf/fzf-tab
 zinit ice wait depth=1; zinit light MichaelAquilina/zsh-you-should-use
 zinit ice wait depth=1; zinit light z-shell/zsh-eza
+zinit ice wait depth=1; zinit light zsh-users/zsh-completions
